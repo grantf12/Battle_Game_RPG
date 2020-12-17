35fadfc0d3935bd115d5d62ea96f284ba576eb69
Baker Quillin <bakerquil@gmail.com>
2020-12-17 15:12:07 -0500

    fixed eslint errors

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,28 +1,27 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function (app) {
-
-  app.get("/", function (req, res) {
+module.exports = function(app) {
+  app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     // res.sendFile(path.join(__dirname, "../views/signup.html"));
-    res.render("welcome")
+    res.render("welcome");
   });
 
-  app.get("/login", function (req, res) {
+  app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../views/login.html"));
   });
-  app.get("/signup", function (req, res) {
+  app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
@@ -32,15 +31,13 @@ module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function (req, res) {
-    console.log(req.user)
-    res.render("members-info", req.user)
+  app.get("/members", isAuthenticated, (req, res) => {
+    console.log(req.user);
+    res.render("members-info", req.user);
   });
 
-  app.get("/battle", isAuthenticated, function (req, res) {
+  app.get("/battle", isAuthenticated, (req, res) => {
     console.log(req.user);
     res.render("battle-info", req.user);
   });
-
-  
 };
