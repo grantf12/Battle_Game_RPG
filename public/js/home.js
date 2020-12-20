9f161c7050ee7852794707241fb2d7960580e08c
Baker Quillin <bakerquil@gmail.com>
2020-12-20 13:37:07 -0500

    updated routes, added stylesheets to handlebars files, all routes are working, flow of web pages is also

---
+++ public/js/home.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  $("#battle-button").click(() => {
+    window.location.href = "/character-select";
+  });
+  $("#signout-button").click(() => {
+    window.location.href = "/login";
+  });
+});
